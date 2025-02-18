fb10502e25d624395d668cc85f776de619b06fc5
Christopher Lates <christopherlates@gmail.com>
2025-02-17 22:29:06 -0500

    Change vite build settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ command }) => {
+  let baseConfig = {
+    base: "/",
+    plugins: [react(), tailwindcss()],
+  };
+
+  if (command === "build") {
+    baseConfig = {
+      ...baseConfig,
+      base: "/v-sol-explorer/",
+    };
+  }
+  return baseConfig;
+});
